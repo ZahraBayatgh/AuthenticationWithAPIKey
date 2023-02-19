54fcfdcd1540a3be1bc9c956cfdc971978b3982b
Zahra Bayat <58395585+ZahraBayatgh@users.noreply.github.com>
2023-02-19 07:20:35 +0330

    Add project files.

---
+++ AuthenticationWithAPIKey/APIKeyMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace AuthenticationWithAPIKey
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class APIKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration _configuration;
+
+        public APIKeyMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _configuration = configuration;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (!httpContext.Request.Headers.TryGetValue(
+                "api-key", out var apikeyHeader))
+            {
+                httpContext.Response.StatusCode = 401;
+                await httpContext.Response.WriteAsync("UnAutorize");
+                return;
+            }
+            var apikeyvalue = _configuration.GetValue<string>("ApiKey");
+            if (!apikeyvalue.Equals(apikeyHeader))
+            {
+                httpContext.Response.StatusCode = 401;
+                await httpContext.Response.WriteAsync("Invalid Api Key");
+                return;
+            }
+            await  _next(httpContext);
+        }
+    }
+
+  
+}
